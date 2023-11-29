16180aa5d52b0b1076b91614bf92c360a058dc7c
sofivasa <sofivasa@myuax.com>
2023-11-29 22:23:48 +0100

    ejercicio6

--- ex6.cpp
+++ ex6.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <boost/lexical_cast.hpp>
 
+int main (){
+    std::string i = "123456789";
+            try {
+                //Cambiar string a int
+                int i = boost::lexical_cast<int>(i);
+                std::cout<<"Numero: "<<i<<std::endl;
+            }catch (boost::bad_lexical_cast &e){
+                //error
+std::cout<<"Error: "<<e.what()<<std::endl;
+
+            }
+            return 0;
+}
