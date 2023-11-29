c11190f802b34c447ce4c9994e52257f2c05d0b0
sofivasa <sofivasa@myuax.com>
2023-11-29 20:43:26 +0100

    errorejercicio2

--- ex2.cpp
+++ ex2.cpp
@@ -6,11 +6,11 @@ int main() {
     //Lista de numeros decimalea (float)
     std::list<float> lista={2.5,3.5,8.6,9.7};
     //Agragar numero
-    std::stack<float>pila (lista);
-
+    std::stack<float, std::list<float>> pila (lista);
 //Usar bucle while para imprimir la lista
 while (!pila.empty()){
     std::cout<<pila.top()<<std::endl;
     pila.pop();
 }
+return 0;
 }
