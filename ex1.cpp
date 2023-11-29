7fcf7bd085c8ccc03855fbb75d31e7e1a47ff77d
sofivasa <sofivasa@myuax.com>
2023-11-29 20:19:00 +0100

    correcciondeerrorycomentariosex1

--- ex1.cpp
+++ ex1.cpp
@@ -2,15 +2,16 @@
 #include<vector>
 
 int main () {
+//Crear un vector de enteros
+    std::vector<int> numeros = {10, 20, 30, 40, 50};
+//Agregar elemento al final del vector
+    numeros.push_back(25);
+    numeros.push_back(35);
+//Agrgar numero en la segunda posicion
+    std::cout << "Numeros del vector: " << std::endl;
+    for (int i = 0; i < numeros.size(); i++) {
+        std::cout << numeros[i] << std::endl;
 
-std :: vector <int> numeros = {10,20,30,40,50};
-
-numeros.pushback(25);
-numeros.pushback(35);
-
-std:: cout << "Numeros del vector: "<< std::endl;
-for(int i=0; i<numeros.size(); i++){
-    std::cout << numeros[i] << std::endl;
-
+    }
+    return 0;
 }
-return 0;
