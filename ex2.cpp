b317796b8ddb12ba57d80334eb557944d4776ee7
sofivasa <sofivasa@myuax.com>
2023-11-29 20:34:42 +0100

    commit2ejercicio2

--- ex2.cpp
+++ ex2.cpp
@@ -3,9 +3,14 @@
 #include<stack>
 
 int main() {
+    //Lista de numeros decimalea (float)
     std::list<float> lista={2.5,3.5,8.6,9.7};
+    //Agragar numero
     std::stack<float>pila (lista);
 
-    
-
+//Usar bucle while para imprimir la lista
+while (!pila.empty()){
+    std::cout<<pila.top()<<std::endl;
+    pila.pop();
+}
 }
