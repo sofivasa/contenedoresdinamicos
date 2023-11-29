49edd418e15582a24abf0c8cdf504e96df077aef
sofivasa <sofivasa@myuax.com>
2023-11-29 21:09:47 +0100

    comentariosejercicio3

--- ex3.cpp
+++ ex3.cpp
@@ -2,12 +2,15 @@
 #include<vector>
 #include<algorithm>
 int main (){
+    //Vector de numeros enteros
     std:: vector<int> numeros= {100, 80, 120,20,50,70} ;
     //Ordenar el vector
     std::sort(numeros.begin(),numeros.end()) ;
+    //Imprimir el vector ordenado
           std ::cout<< "Elementos ordenados: " <<std::endl;
       for(int num : numeros){
            std::cout<<num<<std::endl;
+           //Imprimir el numero mayor del vector 
       int numMax= *std::max_element(numeros.begin(),numeros.end());
       std::cout<<"El numero mayor es: "<<numMax<<std::endl;
   }
